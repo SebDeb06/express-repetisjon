27cd1e57f7134927373a3ed92cad7d8682f5df54
SebDeb06 <sesa@hopvgskole.no>
2025-03-03 13:35:13 +0100

    Jeg har lagd til en statisk rute til public mappen.

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send("Welcome to root URL of Server");
+});
 
 app.listen(PORT, (error) =>{
     if(!error)
